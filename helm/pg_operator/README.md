fde4bea93622bb5100b1bd4fbaab37e54387dc85
Connor Dibble <connor.dibble@scootscience.com>
2023-01-24 18:14:35 -0800

    adds zalando postgres operator helm and postgres cluster manifests and deployment files

---
+++ helm/pg_operator/README.md
@@ -0,0 +1,11 @@
+# Zalando Postgres Operator
+
+## Helm chart
+Deploys the Postgres operator pod(s).
+
+## Postgres manifest
+Deploys the acutal Postgres database cluster.
+
+```bash
+kubectl create -f manifests/minimal-postgres-manifest.yaml
+```
---
+++ helm/pg_operator/deploy.sh
@@ -0,0 +1,19 @@
+install () {
+  # add repo for postgres-operator
+  helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator
+  # install the postgres-operator
+  helm install postgres-operator postgres-operator-charts/postgres-operator
+  # # add repo for postgres-operator-ui
+  # helm repo add postgres-operator-ui-charts https://opensource.zalando.com/postgres-operator/charts/postgres-operator-ui
+  # # install the postgres-operator-ui
+  # helm install postgres-operator-ui postgres-operator-ui-charts/postgres-operator-ui
+}
+
+
+deploy_manifest () {
+  if [[ -z $1 ]]; then
+    kubectl create -f manifests/custom-complete-postgres-manifest.yaml
+  else
+    kubectl create -f $1
+  fi
+}
