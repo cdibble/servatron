a9c8d6db92d50536cf694d2f71f5ae5c4f5679ae
Connor <cddibble@gmail.com>
2023-04-20 21:02:50 -0700

    simplifies nextdrive install for use with docker-compose

--- pi/docker/install.sh
+++ pi/docker/install.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 install () {
-    # install docker
-    curl -sSL https://get.docker.com | sh
-    # install docker-compose
-    curl -SL https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+    sudo apt-get update
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg
+    # Add Docker’s official GPG key:
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    # Use the following command to set up the repository:
+    echo \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
     # add conman user to docker group so we dont need to use root
     sudo usermod -aG docker conman
-
+    newgrp docker 
 }
