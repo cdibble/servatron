69f3c4302c3264cbcca6631a891f570a6cf881b1
Connor <cddibble@gmail.com>
2023-04-20 22:59:38 -0700

    updates to docker-compose to distinguish contianers

--- pi/docker/install.sh
+++ pi/docker/install.sh
@@ -16,8 +16,10 @@ install () {
         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
     # add conman user to docker group so we dont need to use root
     sudo usermod -aG docker conman
-    newgrp docker 
+    newgrp docker
+    # start docker daemon
+    sudo service docker restart
 }
