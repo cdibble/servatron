3698f8af9e656d05bd09911d2bf9c206cea31fbd
Connor <cddibble@gmail.com>
2023-04-23 16:09:18 -0700

    formatting

--- pi/certbot/install.sh
+++ pi/certbot/install.sh
@@ -4,6 +4,11 @@ install () {
     sudo apt-get install certbot
     sudo apt-get install python3-certbot-nginx
     # Get LetsEncrypt certificates. Note: requires forwarding port 80 so challenges work.
-    sudo certbot --nginx -d tuerto.net -d www.tuerto.net -d seafile.tuerto.net -d nc.tuerto.net -d www.seafile.tuerto.net -d www.nc.tuerto.net
+    sudo certbot --nginx \
+        -d tuerto.net \
+        -d www.tuerto.net \
+        -d seafile.tuerto.net \
+        -d nc.tuerto.net \
+        -d www.seafile.tuerto.net -d www.nc.tuerto.net
     sudo certbot renew
 }
