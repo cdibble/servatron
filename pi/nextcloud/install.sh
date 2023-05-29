9920aa8ca80d68436581446564be4ef0a1800480
Connor Dibble <connor.dibble@scootscience.com>
2023-05-28 22:16:46 -0700

    switchtes to system cron for nextcloud background jobs because this seems to be the only really reliable way to do it

--- pi/nextcloud/install.sh
+++ pi/nextcloud/install.sh
@@ -4,3 +4,11 @@ docker_up () {
   sed -i  's/POSTGRES_PASSWORD=nextcloud/'"POSTGRES_PASSWORD=$NEXTCLOUD_POSTGRES_PASSWORD"'/' ./docker-compose.yml
   sudo docker-compose up -d
 }
+
+setup_cron () {
+  # use system cron (root user) to run docker exec command (as www-data user) for nextcloud background jobs.
+  sudo su -
+  cronline="*/5 * * * * docker exec -u www-data nextcloud_nextcloud_1  php ./cron.php" 
+  (crontab -u $(whoami) -l; echo "$cronline" ) | crontab -u $(whoami) -
+  exit
+}
