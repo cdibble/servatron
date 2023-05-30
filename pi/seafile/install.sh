27ae991a1baba90b5f009e91989a2c59201a6afa
Connor Dibble <connor.dibble@scootscience.com>
2023-05-30 08:39:18 -0700

    seafile fuse mount steps

--- pi/seafile/install.sh
+++ pi/seafile/install.sh
@@ -6,3 +6,15 @@ docker_up () {
   sed -i  's/=db_dev/'"=$SEAFILE_DB_PASSWORD"'/' ./docker-compose.yml
   sudo docker-compose up -d
 }
+
+mount_seafuse_filesystem () {
+   docker exec -ti seafile bash -c 'cd /opt/seafile/seafile-server-latest/ &&  ./seaf-fuse.sh start /seafile-fuse'
+}
+
+create_container_user () {
+  # supposedly you can run as non-root but I have not yet gotten this to work.
+  # thes would be for adding non-root user to the container...
+  # groupadd --gid 8000 conman
+  # useradd --home-dir /home/conman --create-home --uid 8000 --gid 8000 --shell /bin/sh --skel /dev/null conman
+}
+
