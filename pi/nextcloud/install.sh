815463846a784956938c6fabeeedef6b4b739bc4
Connor Dibble <connor.dibble@scootscience.com>
2023-05-30 08:40:39 -0700

    a few more attempted udpates at using external storage

--- pi/nextcloud/install.sh
+++ pi/nextcloud/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# install_host_deps () {
+#   # this is for local storage mounting via the NC app "External Storage Support"
+#   apt-get install smbclient
+# }
+
 docker_up () {
   sed -i  's/POSTGRES_PASSWORD=nextcloud/'"POSTGRES_PASSWORD=$NEXTCLOUD_POSTGRES_PASSWORD"'/' ./docker-compose.yml
   sudo docker-compose up -d
@@ -11,4 +16,9 @@ setup_cron () {
   cronline="*/5 * * * * docker exec -u www-data nextcloud_nextcloud_1  php ./cron.php" 
   (crontab -u $(whoami) -l; echo "$cronline" ) | crontab -u $(whoami) -
   exit
+}
+
+setup_external_storage () {
+  sudo chown -R www-data:www-data /home/conman/nc
+  sudo chmod -R 0750 /home/conman/nc
 }
