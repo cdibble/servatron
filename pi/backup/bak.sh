7fd43fa281a69956c4e9d510a421e1588ed01810
Connor Dibble <connor.dibble@scootscience.com>
2023-06-04 15:17:49 -0700

    docstrings

--- pi/backup/bak.sh
+++ pi/backup/bak.sh
@@ -7,7 +7,7 @@ install () {
 
 # backup databases
 bak_db () {
-	MYSQLPASSWORD="CdA8q7iSYeQ3iPmD9K9ibbo6GLS"
+	MYSQLPASSWORD="PASSWORD" # Example
 	docker exec -it seafile-mysql /bin/bash -c "mkdir -p ~/backup/databases; \
 	mysqldump -h db -uroot -p$MYSQLPASSWORD --opt ccnet_db > ~/backup/databases/ccnet_db.sql.`date +"%Y-%m-%d-%H-%M-%S"`; \
 	mysqldump -h db -uroot -p$MYSQLPASSWORD --opt seafile_db > ~/backup/databases/seafile_db.sql.`date +"%Y-%m-%d-%H-%M-%S"`; \
@@ -20,13 +20,7 @@ bak_data () {
 	sudo rsync -azS /opt/seafile-data/seafile /mnt/bak/seafile/data/
 }
 
-# backup data files
-bak_dat () {
-	rsync -az /data/haiwen /backup/data
-}
-
 # restore databases
-
 restore_db () {
 	mysql -u[username] -p[password] ccnet-db < ccnet-db.sql.2013-10-19-16-00-05
 	mysql -u[username] -p[password] seafile-db < seafile-db.sql.2013-10-19-16-00-20
@@ -42,7 +36,9 @@ mount_bak () {
 	# 
 	sudo mkdir -p /mnt/bak
 	sudo chmod 0777 /mnt/bak
-	sudo mount -t ext4 /dev/sdb2 /mnt/bak -o rw # use -o umask=000
+	# TODO: Look at the device with the /root volume and mount the other device...
+		# b/c whether it is sda or sdb depends on boot order and is seemingly not deterministic
+	sudo mount -t ext4 /dev/sda2 /mnt/bak -o rw # use -o umask=000
 	# auto-mount on boot; dont enable until after first backup just in case this corrupts the boot volume.
-	# echo "/dev/sdb2 /mnt/bak ntfs-3g async,big_writes,noatime,nodiratime,nofail,uid=1000,gid=1000,umask=007,x-systemd.device-timeout=1 0 0" >> /etc/fstab
+	# echo "/dev/sda2 /mnt/bak ntfs-3g async,big_writes,noatime,nodiratime,nofail,uid=1000,gid=1000,umask=007,x-systemd.device-timeout=1 0 0" >> /etc/fstab
 }
